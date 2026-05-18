56e36d0e04ba7650af210883420cab1adca1f6ca
unknown <vllorens32826@alumnos.uemc.es>
2026-05-18 11:29:52 +0200

    Primer commit

---
+++ src/main/java/com/mycompany/practicafinalmtpa/PracticaFinalMTPA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicafinalmtpa;
+
+/**
+ *
+ * @author Veronica
+ */
+public class PracticaFinalMTPA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
