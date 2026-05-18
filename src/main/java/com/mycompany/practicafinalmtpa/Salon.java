4662a31b449964ac46079a4d9caddc686aa9eaea
unknown <vllorens32826@alumnos.uemc.es>
2026-05-19 00:11:48 +0200

    primeras clases salon

---
+++ src/main/java/com/mycompany/practicafinalmtpa/Salon.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.practicafinalmtpa;
+
+/**
+ *
+ * @author Veronica
+ */
+//hay q plantear esto a ver si son como servidores que luego mandan a clientes,
+//o tienen la lista de clientes conectados
+//y es el mismo servidor quien se lo manda a los clientes conectados, o esto son clientes o hilos nose
+//pone q todo tiene q ser comunicacion servidor pero nose
+public abstract class Salon {
+    //tienen q tener una variable para ver cuantos clientes estan conectados  acada uno de ellos o algo
+    
+}
