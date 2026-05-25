eca8a5971545c0c134ed9930653077f229f1c4df
David-clj <davidcallejaalonso@gmail.com>
2026-05-25 15:50:39 +0200

    Inicio de la interfaz del usuario, creados 3 Jframes y resuelto algunas tareas de por medio como la creacion de un main para el cliente

---
+++ src/main/java/com/mycompany/practicafinalmtpa/PracticaFinalMTPA.java
@@ -0,0 +1,8 @@
+package com.mycompany.practicafinalmtpa;
+
+
+    public class PracticaFinalMTPA {
+    public static void main(String[] args) {
+        new FrameCliente().setVisible(true);
+    }
+}
