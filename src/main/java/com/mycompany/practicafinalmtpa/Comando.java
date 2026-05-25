e801efb1a170ba247d0a996063cfe918f50579da
Veronica <vllorens32826@alumnos.uemc.es>
2026-05-25 13:00:48 +0200

    una clase comando

--- src/main/java/com/mycompany/practicafinalmtpa/Comando.java
+++ src/main/java/com/mycompany/practicafinalmtpa/Comando.java
@@ -9,6 +9,6 @@ package com.mycompany.practicafinalmtpa;
  * @author Veronica
  */
 public interface Comando {
-    public void procesarMensaje(String comando);
+    public void procesarMensaje();
     
 }
